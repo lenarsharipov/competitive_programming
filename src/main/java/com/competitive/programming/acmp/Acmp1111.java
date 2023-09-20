b94e463e5a9c985f84b3cab9fa24ba04507ac59c
TETACOMTR <cagri@teta.com.tr>
2023-09-20 10:39:15 +0300

    Upload Acmp1111 solution

---
+++ src/main/java/com/competitive/programming/acmp/Acmp1111.java
@@ -0,0 +1,21 @@
+package com.competitive.programming.acmp;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Acmp1111 {
+    void solve(Scanner in, PrintWriter out) {
+        out.println(in.nextInt() % 100 / 10);
+    }
+
+    void run() {
+        try(Scanner in = new Scanner(System.in);
+            PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Acmp1111().run();
+    }
+}
